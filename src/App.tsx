5c6b12458cd53b700d277896bbd89675f7aeaeb7
Tetiana5Buria <poriniya@gmail.com>
2025-10-14 11:34:16 +0300

    hashRouter in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Board from './pages/Board/Board';
 import Home from './pages/Home/Home';
 import CustomToaster from '../src/pages/Board/components/CustomToster/CustomToaster';
 function App(): React.ReactElement {
-  const basename = process.env.PUBLIC_URL;
+  /* const basename = process.env.PUBLIC_URL; */
 
   return (
-    <Router basename={basename}>
+    <Router /* basename={basename} */>
       <CustomToaster />
       <Routes>
         <Route path="/board/:board_id" element={<Board />} />
